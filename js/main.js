715773b732fa5d3d137b7418e0ecbb533c7d0df7
Zymantasdan <z.dansevicius@gmail.com>
2020-12-03 13:20:53 +0200

    pet

--- js/main.js
+++ js/main.js
@@ -1,36 +1,11 @@
-import { Dog } from './Dog.js';
+import { Dog } from './animals/Dog.js';
+// import { Cat } from './animals/Cat.js';
 
-const rexius = new Dog('Rexas', 'rudas', 'vokieciu aviganis');
-const brisius = new Dog('Brisius', 'pilkas', 'lietuviu');
+const rikis = new Dog('Rikis', 'baltas');
+// const rainis = new Cat('Rainis', 'juodas')
 
+rikis.sound();
+// rainis.sound();
 
-// jei noriu issitraukti brisius vardas
-console.log(rexius);
-console.log(brisius);
-
-console.log(rexius.breed);
-console.log(brisius.breed);
-console.log('.........................');
-
-// metodas introduce-prisistatyk, tarsi funkcija ivestis clonsole.log isvestis cia.
-rexius.introduce();
-brisius.introduce();
-
-console.log(rexius.speed);
-
-rexius.move(5);
-rexius.move(10);
-rexius.move(20);
-rexius.move(30);
-rexius.move(40);
-rexius.move(100);
-
-brisius.move(10);
-brisius.move(2);
-brisius.move(50);
-brisius.move(100);
-
-// atspausdink sarasa koki zmogu suo myli.
-
-rexius.kaMyli('Zymantas');
-rexius.kaMyli('Andrius');
+rikis.introduce();
+// rainis.introduce();
---
+++ js/oldDog/main.js
@@ -0,0 +1,36 @@
+import { Dog } from './Dog.js';
+
+const rexius = new Dog('Rexas', 'rudas', 'vokieciu aviganis');
+const brisius = new Dog('Brisius', 'pilkas', 'lietuviu');
+
+
+// jei noriu issitraukti brisius vardas
+console.log(rexius);
+console.log(brisius);
+
+console.log(rexius.breed);
+console.log(brisius.breed);
+console.log('.........................');
+
+// metodas introduce-prisistatyk, tarsi funkcija ivestis clonsole.log isvestis cia.
+rexius.introduce();
+brisius.introduce();
+
+console.log(rexius.speed);
+
+rexius.move(5);
+rexius.move(10);
+rexius.move(20);
+rexius.move(30);
+rexius.move(40);
+rexius.move(100);
+
+brisius.move(10);
+brisius.move(2);
+brisius.move(50);
+brisius.move(100);
+
+// atspausdink sarasa koki zmogu suo myli.
+
+rexius.kaMyli('Zymantas');
+rexius.kaMyli('Andrius');
