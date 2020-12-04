6a412b8b3446837cc842b3064d308e6a0541db5c
Zymantasdan <z.dansevicius@gmail.com>
2020-12-04 10:35:42 +0200

    papildyta

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 import { Dog } from './animals/Dog.js';
-// import { Cat } from './animals/Cat.js';
+import { Cat } from './animals/Cat.js';
 
 const rikis = new Dog('Rikis', 'baltas');
-// const rainis = new Cat('Rainis', 'juodas')
+const rainis = new Cat('Rainis', 'juodas')
 
-rikis.sound();
-// rainis.sound();
+// rikis.sound();
+rainis.sound();
 
-rikis.introduce();
-// rainis.introduce();
+// rikis.introduce();
+rainis.introduce();
